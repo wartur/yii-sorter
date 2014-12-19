b6e39e4db57e52094fc3672eb4e5ad3f8f7961a7
gwartur <gwartur@gmail.com>
2014-12-19 19:53:29 +0300

    Change changelog

--- tests/env/schema/sortest.sql
+++ tests/env/schema/sortest.sql
@@ -1,8 +1,8 @@
 DROP TABLE IF EXISTS `sortest`;
 CREATE TABLE IF NOT EXISTS `sortest` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `sort` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `sort` (`sort`)
+	`id`	int(11)			NOT NULL	AUTO_INCREMENT,
+	`name`	varchar(255)	NOT NULL,
+	`sort`	int(11)			NOT NULL,
+	PRIMARY KEY (`id`),
+	UNIQUE KEY `sort` (`sort`)
 ) ENGINE=MEMORY  DEFAULT CHARSET=utf8;
